91e0289cf08c3df06030910d090f8fddc11ce500
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-10-05 17:22:32 +0500

    Task1

---
+++ Tyuiu.SafronovVE.Sprint5.Task1.V17.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SafronovVE.Sprint5.Task1.V17.Lib;
+namespace Tyuiu.SafronovVE.Sprint5.Task1.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistFile()
+        {
+            string path = @"C:\Users\safro\AppData\Local\Temp\OutPutFileTask1.txt";
+            
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(true, fileExist);
+
+        }
+    }
+}
