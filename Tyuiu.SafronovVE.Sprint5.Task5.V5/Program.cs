6c2f54f4238c28b168f0a351d2e6f8674565a7dc
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-10-10 21:58:24 +0500

    Task5

--- Tyuiu.SafronovVE.Sprint5.Task5.V5/Program.cs
+++ Tyuiu.SafronovVE.Sprint5.Task5.V5/Program.cs
@@ -4,6 +4,7 @@ internal class Program
     private static void Main(string[] args)
     {
         DataService ds = new DataService();
+        File.WriteAllText(Path.Combine(Path.GetTempPath(), "InPutDataFileTask5V5.txt"), "-3.09 3 3 7.48 -3.22 17.29 8 -4 0.83 14.18 -6 8.15 -8.7 -3.06 20 -4 15.82 -10 9 -3");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
