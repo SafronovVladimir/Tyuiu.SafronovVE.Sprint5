0f34cdbe864d9e08a3a4b59501a20e1262b0b175
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-10-10 19:39:17 +0500

    Task4

--- Tyuiu.SafronovVE.Sprint5.Task4.V14.Lib/DataService.cs
+++ Tyuiu.SafronovVE.Sprint5.Task4.V14.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.SafronovVE.Sprint5.Task4.V14.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            File.WriteAllText(path, "2.24");
+            File.WriteAllText(path, "4.21");
 
             string strX = File.ReadAllText(path);
             IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
