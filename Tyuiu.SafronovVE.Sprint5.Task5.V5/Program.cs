aacc9db34c88a3eea55b09d4de84a5efacc5649e
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-10-10 22:01:50 +0500

    Task5

--- Tyuiu.SafronovVE.Sprint5.Task5.V5/Program.cs
+++ Tyuiu.SafronovVE.Sprint5.Task5.V5/Program.cs
@@ -3,13 +3,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        File.WriteAllText(Path.Combine($"{Directory.GetCurrentDirectory()}", "InPutDataFileTask5V5.txt"), "-3.09 3 3 7.48 -3.22 17.29 8 -4 0.83 14.18 -6 8.15 -8.7 -3.06 20 -4 15.82 -10 9 -3");
         DataService ds = new DataService();
-        File.WriteAllText(Path.Combine(Path.GetTempPath(), "InPutDataFileTask5V5.txt"), "-3.09 3 3 7.48 -3.22 17.29 8 -4 0.83 14.18 -6 8.15 -8.7 -3.06 20 -4 15.82 -10 9 -3");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask5V5.txt");
+        string path = Path.Combine($"{Directory.GetCurrentDirectory()}", "InPutDataFileTask5V5.txt");
 
         Console.WriteLine("Файл находится: " + path);
 
