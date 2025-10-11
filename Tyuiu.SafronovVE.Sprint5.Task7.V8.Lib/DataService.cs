34e7e31f682a7c32312f6f59f512b415a2af35f2
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-10-11 17:21:15 +0500

    Task7

--- Tyuiu.SafronovVE.Sprint5.Task7.V8.Lib/DataService.cs
+++ Tyuiu.SafronovVE.Sprint5.Task7.V8.Lib/DataService.cs
@@ -19,10 +19,9 @@ namespace Tyuiu.SafronovVE.Sprint5.Task7.V8.Lib
 
             if (!File.Exists(path))
             {
-                File.WriteAllText(path, "");
+                File.WriteAllText(path, "Привет, World! This моя Первая ПРограмма.");
             }
 
-            File.WriteAllText(path, "Привет, World! This моя Первая ПРограмма.");
             string str = File.ReadAllText(path);
             string res = "";
             foreach (char i in str)
